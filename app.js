2efba5581e3c9cd1fceb890eed23377c67f4f16f
Liza Kobtseva <elizavetakobtsevaa@yandex.ru>
2023-12-22 00:01:34 +0300

    test fix2

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const router = require('./routes');
 
 const app = express();
-
 app.use(bodyParser.json());
 
-const { PORT = 9090, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 async function connect() {
   try {
-    await mongoose.set('strictQuery', false);
-    await mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-    console.log(`App connected ${MONGO_URL}`);
-    app.listen(PORT);
-    console.log(`App listening on port ${PORT}`);
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`App connected to ${MONGO_URL}`);
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
   } catch (err) {
-    console.log(err);
+    console.error('Error connecting to MongoDB:', err);
   }
 }
-app.listen(9090);
 
 app.use((req, res, next) => {
   req.user = {
@@ -29,9 +29,12 @@ app.use((req, res, next) => {
   };
   next();
 });
+
 // подключаем роуты
 app.use(router);
+
 app.get('/', (req, res) => {
   res.send('hkhh');
 });
+
 connect();
